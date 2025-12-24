824ac391f41a1cc5d3dc0d20296cdeb7f6373d02
Eduardo Alves Nunes <eduardo.nunes16@fatec.sp.gov.br>
2025-12-24 21:07:23 +0000

    feat:endpoint buscarIdProduto

--- src/main/java/com/example/demo/controller/ProdutoController.java
+++ src/main/java/com/example/demo/controller/ProdutoController.java
@@ -8,13 +8,14 @@ import com.example.demo.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 
@@ -26,20 +27,29 @@ public class ProdutoController {
     private ProdutoRepository produtoRepository;
 
     @PostMapping("/salvar")
-    public String salvarProduto(@RequestBody Produto produto) {
+    public Produto salvarProduto(@RequestBody Produto produto) {
         String id = UUID.randomUUID().toString();
 
         produto.setId(id);
         produtoRepository.save(produto);
         
-        return produto.toString();
+        return produto;
     }
 
     @GetMapping("/")
-    public List<Produto> getMethodName() {
-        List<Produto> produtos = produtoRepository.findAll();
+    public List<Produto> listarProduto() {
+        final List<Produto> produtos = produtoRepository.findAll();
 
         return produtos;
+    }
+
+    @GetMapping("/{id}")
+    public Produto buscarIdProduto(@PathVariable String id){
+        Optional<Produto> produto = produtoRepository.findById(id);
+
+        if(produto.isPresent()){
+            return produto.get();
+        } else return null;
     }
     
     
