735732ab141517bd9f21dd921427b4f1d85ad92a
Eduardo Alves Nunes <eduardo.nunes16@fatec.sp.gov.br>
2025-12-24 21:38:16 +0000

    feat:endpoint atualizarIdProduto

--- src/main/java/com/example/demo/controller/ProdutoController.java
+++ src/main/java/com/example/demo/controller/ProdutoController.java
@@ -14,8 +14,11 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 
@@ -45,17 +48,26 @@ public class ProdutoController {
 
     @GetMapping("/{id}")
     public Produto buscarIdProduto(@PathVariable String id){
-        Optional<Produto> produto = produtoRepository.findById(id);
+        Optional<Produto> produtoBanco = produtoRepository.findById(id);
 
-        if(produto.isPresent()){
-            return produto.get();
+        if(produtoBanco.isPresent()){
+            return produtoBanco.get();
         } else return null;
     }
 
-    @PostMapping("/delete/{id}")
-    public void deletarIdProdutos(@PathVariable String id){
+    @DeleteMapping("{id}")
+    public void deletarIdProduto(@PathVariable String id){
         produtoRepository.deleteById(id);
     }
     
-    
+    @PutMapping("{id}")
+    public void atualizarIdProduto(@PathVariable String id, @RequestBody Produto produto) {
+        Optional<Produto> produtoBanco = produtoRepository.findById(id);
+        
+        if(produtoBanco.isPresent()){
+            produto.setId(id);
+
+            produtoRepository.save(produto);
+        }
+    }
 }
