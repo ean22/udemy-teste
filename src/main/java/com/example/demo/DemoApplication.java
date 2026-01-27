d555fb016c2b42e94eb98dd230173a11aec6d6ee
Eduardo Alves Nunes <eduardo.nunes16@fatec.sp.gov.br>
2026-01-27 18:18:54 +0000

    feat: configuratioDataSourcewip

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,9 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.ConfigurableEnvironment;
+
 
 @SpringBootApplication
 @EnableConfigurationProperties
---
+++ src/main/java/com/example/demo/configuration/DataBaseConfiguration.java
@@ -0,0 +1,39 @@
+package com.example.demo.configuration;
+
+import java.sql.DriverManager;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class DataBaseConfiguration {
+    
+    @Value("${spring.datasource.url}")
+    String url;
+
+    @Value("${spring.datasource.username}")
+    String username;
+
+    @Value("${spring.datasource.password}")
+    String password;    
+
+    @Value("${spring.datasource.driver-class-name}")
+    String driver;
+    
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource ds = new DriverManagerDataSource()
+        ds.setUrl(url);
+        ds.setUsername(username);
+        ds.setPassword(password);
+        ds.setDriverClassName(driver);
+
+        return ds;
+    }
+
+    
+}
