c55a63f2e131b44648101faabd6763b84fcae28b
Eduardo Alves Nunes <eduardo.nunes16@fatec.sp.gov.br>
2026-01-29 22:35:13 +0000

    wip: add Livro class

---
+++ src/main/java/com/example/demo/configuration/model/GeneroEnum.java
@@ -0,0 +1,11 @@
+package com.example.demo.configuration.model;
+
+public enum GeneroEnum {
+    FICCAO("c"),
+    FANTASIA("b"),
+    MISTERIO("a");
+
+    GeneroEnum(String a){
+
+    }
+}
--- src/main/java/com/example/demo/configuration/model/Livro.java
+++ src/main/java/com/example/demo/configuration/model/Livro.java
@@ -1,10 +1,21 @@
 package com.example.demo.configuration.model;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Entity
 @Table( name = "livro")
 public class Livro {
-    
+    private UUID id;
+    private String isbn;
+    private String titulo;
+    @Column( name = "data_publicacao" )
+    private LocalDate dataPublicação;
+    private String genero = GeneroEnum.MISTERIO;
+
+
 }
