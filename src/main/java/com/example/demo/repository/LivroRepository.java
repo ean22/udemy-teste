49f3eedc643d2bd13d9cd91fdfb1f933d6d00bc0
Eduardo Alves Nunes <eduardo.nunes16@fatec.sp.gov.br>
2026-02-17 01:01:57 +0000

    feat: add LivroRepository.findByAutor

--- src/main/java/com/example/demo/repository/LivroRepository.java
+++ src/main/java/com/example/demo/repository/LivroRepository.java
@@ -1,11 +1,13 @@
 package com.example.demo.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.Livro;
+import com.example.demo.model.Autor;
 
 public interface LivroRepository extends JpaRepository<Livro, UUID>{
-    
+    List<Livro> findByAutor(Autor autor);
 }
