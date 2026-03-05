a958d6ea4731d06da3dfcbc54d173fe887572b60
Eduardo Alves Nunes <eduardo.nunes16@fatec.sp.gov.br>
2026-03-05 00:21:25 +0000

    refactor: rm unused imports

--- src/main/java/com/example/demo/model/Livro.java
+++ src/main/java/com/example/demo/model/Livro.java
@@ -2,12 +2,10 @@ package com.example.demo.model;
 
 import java.util.UUID;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
--- src/main/java/com/example/demo/service/AutorService.java
+++ src/main/java/com/example/demo/service/AutorService.java
@@ -1,7 +1,5 @@
 package com.example.demo.service;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 
 import com.example.demo.dto.AutorDTO;
